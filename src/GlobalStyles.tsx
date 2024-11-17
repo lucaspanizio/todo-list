78cc3170d2db884c21e8d46beba0f905b632d309
Lucas Panizio <lucas_panizio@hotmail.com>
2024-11-17 03:08:38 -0300

    :lipstick: style: changed scrollbar style

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -23,6 +23,27 @@ export const GlobalStyles = createGlobalStyle`
     outline: transparent;
   }
 
+  ::-webkit-scrollbar {
+    width: 12px;
+    height: 12px;
+    box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.9);
+  }
+
+  ::-webkit-scrollbar-track {
+    background: var(--gray-700);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: var(--gray-500);
+    border: 1px solid var(--gray-900);
+    cursor: grabbing;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: var(--gray-400);
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -30,8 +51,8 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background: var(--gray-600);
     color: var(--gray-100);
+    background: var(--gray-600);
     -webkit-font-smoothing: antialiased;
   }
 
